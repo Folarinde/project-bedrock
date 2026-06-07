4da864df72ce95413ca33f92afcbad867d665057
Folarinde <folarinde.olusola@gmail.com>
2026-06-07 19:09:10 +0100

    Configured app manifests and temporarily removed cloudwatch configurations

--- terraform/modules/eks/outputs.tf
+++ terraform/modules/eks/outputs.tf
@@ -42,13 +42,3 @@ output "alb_controller_role_arn" {
   description = "IAM role ARN for AWS Load Balancer Controller"
   value       = aws_iam_role.alb_controller.arn
 }
-
-output "cloudwatch_agent_role_arn" {
-  description = "IAM role ARN for CloudWatch agent"
-  value       = aws_iam_role.cloudwatch_agent.arn
-}
-
-output "fluent_bit_role_arn" {
-  description = "IAM role ARN for FluentBit log forwarder"
-  value       = aws_iam_role.fluent_bit.arn
-}
